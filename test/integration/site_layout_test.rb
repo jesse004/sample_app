fce0641213a61badf8b5310364693da1eaf9eb20
Jesse <jessefister@yahoo.com>
2015-05-21 01:41:35 +0000

    Finish layout and routes

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+  
+  test "layout links" do
+    get root_path
+    assert_template 'static_pages/home'
+    assert_template "a[href=?]", root_path
+    assert_template "a[href=?]", help_path
+    assert_template "a[href=?]", about_path
+    assert_template "a[href=?]", contact_path
+  end
+end
